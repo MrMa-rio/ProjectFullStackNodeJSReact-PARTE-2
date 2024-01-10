9ba420e19a98297ece4ff2b78d78cf640ffceffc
Mario Alberto <112214759+MrMa-rio@users.noreply.github.com>
2024-01-10 17:11:45 -0300

    feat: adding styling to page

--- projectfullstacknodejsreact-parte-2/src/app/site/carrinho/page.tsx
+++ projectfullstacknodejsreact-parte-2/src/app/site/carrinho/page.tsx
@@ -1,12 +1,15 @@
 export default function page(){
     return(
         <>
-            <div className="flex justify-center items-center px-10 pt-32" >
-                <div className="flex w-[80%] h-[60vh] bg-black text-white"> {/* apos preencher colocar o h-fit */}
-                    <div className="w-full">
+            <div className="flex justify-center items-center px-10 pt-28" >
+                <div className="flex items-center w-[80%] h-[70vh] border-2 border-gray-200 rounded-2xl"> {/* apos preencher colocar o h-fit */}
+                    <div className="w-full h-full">
 
                     </div>
-                    <div className="w-full">
+                    <div className="w-0.5 h-[80%] bg-gray-400 rounded-xl"></div>
+                    <div className="flex flex-col w-1/2 h-full pt-8 p-4">
+                        <h2 className="text-xl">SUBTOTAL</h2>
+                        {/* <div className="w-full h-0.5 self-center bg-black rounded-xl"></div> */}
 
                     </div>
                 </div>
